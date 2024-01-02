bb4349b7d6f636c0d324b3f446cb8a87a3a871da
Duc Luu <96047600+DG2609@users.noreply.github.com>
2024-01-03 06:43:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ client = commands.Bot(command_prefix='?',intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("OTg5ODg3ODIwMTY2NDc5ODgz.GS-MEN.cLgmUk2j_MBlhZOvdptesTXy92IQGLVf6U1uVU")
+client.run("")
